fc0f1d9ebf5f8650af5b2a394bab58579f2a21d7
NanaLuv <lovenana961@gmail.com>
2026-04-13 19:51:03 +0000

    Fix port binding for Render

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ async function testConnection() {
   }
 }
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log("Server running on port " + port);
   testConnection();
 });
